fa915199ab303e1eafa344f9afa80841988a2418
Wilhelm Rauston <wilhelm.rauston@gmail.com>
2025-09-09 16:28:27 +0200

    start

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -1,6 +1,27 @@
 package kademlia
 
+import (
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+
 type Kademlia struct {
+	me Contact
+	network *Network
+	rt *RoutingTable
+	k int
+}
+
+func newKademliaNode(address string, me string) (kademlia Kademlia){
+	sha1.Sum([]byte(me))
+	sha1 := sha1.Sum([]byte(me))
+	key := hex.EncodeToString(sha1[:])
+	id := NewKademliaID(key)
+	kademlia.me = NewContact(id, address)
+	kademlia.rt = NewRoutingTable(kademlia.me)
+	kademlia.network = &Network{&kademlia}
+	return
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
